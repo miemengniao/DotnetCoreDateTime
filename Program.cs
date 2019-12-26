2562d40d17517d6b9be05d0de00a21a0c2f33a3c
灭蒙鸟 <wangyz@trial-data.com>
2019-12-26 07:00:57 +0000

    create project structure

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace DotnetCoreDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Trial");
+            
+        }
+    }
+}
