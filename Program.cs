9d27baae530d5b967d8b3a0e2f7eeca2f76247bd
灭蒙鸟 <wangyz@trial-data.com>
2019-12-28 14:05:03 +0000

    更新系统

--- Program.cs
+++ Program.cs
@@ -1,14 +1,43 @@
 using System;
+using System.Collections.ObjectModel;
 
 namespace DotnetCoreDateTime
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Good Trial");
+            DateTime thisDate = new DateTime(2007, 3, 10, 0, 0, 0);
+            DateTime dstDate = new DateTime(2007, 6, 10, 0, 0, 0);
+            DateTimeOffset thisTime;
             
+            thisTime = new DateTimeOffset(dstDate, new TimeSpan(-7, 0, 0));
+            ShowPossibleTimeZones(thisTime);
+
+            thisTime = new DateTimeOffset(thisDate, new TimeSpan(-6, 0, 0));  
+            ShowPossibleTimeZones(thisTime);
+
+            thisTime = new DateTimeOffset(thisDate, new TimeSpan(+1, 0, 0));
+            ShowPossibleTimeZones(thisTime);
         }
+
+        private static void ShowPossibleTimeZones(DateTimeOffset offsetTime)
+        {
+            TimeSpan offset = offsetTime.Offset;
+            ReadOnlyCollection<TimeZoneInfo> timeZones;
+                    
+            Console.WriteLine("{0} could belong to the following time zones:", 
+                                offsetTime.ToString());
+            // Get all time zones defined on local system
+            timeZones = TimeZoneInfo.GetSystemTimeZones();     
+            // Iterate time zones 
+            foreach (TimeZoneInfo timeZone in timeZones)
+            {
+                // Compare offset with offset for that date in that time zone
+                if (timeZone.GetUtcOffset(offsetTime.DateTime).Equals(offset))
+                    Console.WriteLine("   {0}", timeZone.DisplayName);
+            }
+            Console.WriteLine();
+        } 
     }
 }
